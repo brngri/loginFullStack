a552b9910a6af26f3abf6bfc025571595abd8257
brngri <brunaonegri@gmail.com>
2025-01-29 10:42:34 -0300

    tela de signup criada

--- login-page/src/app/app.routes.ts
+++ login-page/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
 
 export const routes: Routes = [
-    {path:"login",component:LoginComponent}
+    {path:"", redirectTo:"login", pathMatch: 'full'},
+    {path:"login",component:LoginComponent},
+    {path:"signup", component:SignupComponent}
 ];
