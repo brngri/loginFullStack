d33105207586b175907afb2eed14ac4efc24f275
brngri <brunaonegri@gmail.com>
2025-01-29 10:13:25 -0300

    Tela de login concluida

--- login-page/src/app/components/pages/login/login.component.ts
+++ login-page/src/app/components/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { PrimaryInputComponent } from '../../primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -17,9 +18,11 @@ export class LoginComponent {
 
   loginForm!: FormGroup;
 
+
   constructor(
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private toastr: ToastrService
   ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -29,8 +32,8 @@ export class LoginComponent {
   
   submit(){
     this.loginService.login(this.loginForm.value.email,  this.loginForm.value.password).subscribe({
-      next: () => console.log("sucesso"),
-      error: () => console.log("error")
+      next: () =>  this.toastr.success("Login feito com sucesso!"),
+      error: () => this.toastr.error("Erro inesperado! Tente novamente mais tarde")
     })
 
   }
