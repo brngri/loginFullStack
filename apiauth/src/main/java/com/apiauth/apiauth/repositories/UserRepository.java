6a44b866a709292bf6bcd70754329d0957a13939
brngri <brunaonegri@gmail.com>
2025-01-30 13:41:02 -0300

    SecurityFilter criado

--- apiauth/src/main/java/com/apiauth/apiauth/repositories/UserRepository.java
+++ apiauth/src/main/java/com/apiauth/apiauth/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.apiauth.apiauth.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.apiauth.apiauth.domain.user.User;
 
-public interface UserRepositor extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
     
 }
