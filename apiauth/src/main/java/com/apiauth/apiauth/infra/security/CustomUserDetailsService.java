7c32e2aadaeb463cff00b1fa163016f44d77ee64
brngri <brunaonegri@gmail.com>
2025-01-30 13:50:27 -0300

    CustomUserDetailsService criado

---
+++ apiauth/src/main/java/com/apiauth/apiauth/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.apiauth.apiauth.infra.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.apiauth.apiauth.domain.user.User;
+import com.apiauth.apiauth.repositories.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+    
+}
